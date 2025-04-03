1df88e7bc484c6fea9e7bbfbe3701f105e68a857
Cindy Mae Ngoho <cindymaen17@gmail.com>
2025-04-03 21:47:08 +0900

    still have an error of migrating

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "destroy users, battles, challenges..."
+CodeBaUser.destroy_all
+CodeBaBattle.destroy_all
+CodeBaChallenge.destroy_all
+puts "Done"
+
+puts "create 2 users..."
+cindy = CodeBaUser.create!(email: "cindy@me.com", password: "secret4sure")
+kaitlyn = CodeBaUser.create!(email: "kaitlyn@me.com", password: "secret4sure")
+puts "Done"
+puts "Create 1 battle and 2 challenges..."
+battle = CodeBaBattle.create!(name: "Ruby strikes back!", user: cindy)
+CodeBaChallenge.create!(
+  title: "Challenge 1",
+  description: "return an empty array",
+  code_ba_battle: battle
+)
+CodeBaChallenge.create!(
+  title: "Challenge 2",
+  description: "return an empty string",
+  code_ba_battle: battle
+)
+CodeBaChallenge.create!(
+  title: "Challenge 3",
+  description: "return an empty hash",
+  code_ba_battle: battle
+)
+puts "Done"
